9fe1ef326526381e2ed499b98fcdee70d7a68c62
Filip Krawiec <FIlip.Krawiec@gmail.com>
2022-04-29 01:53:36 +0200

    Updated App.tsx and PresentationScreen impl

---
+++ src/time-utils.ts
@@ -0,0 +1,26 @@
+type Duration = {
+  hours: number
+  minutes: number
+  seconds: number
+}
+
+export function getDurationString(date1: Date, date2: Date) {
+  const secondsToEvent = (date1.getTime() - date2.getTime()) / 1000
+  return {
+    hours: Math.max(0, Math.floor(secondsToEvent / 3600)) % 24,
+    minutes: Math.max(0, Math.floor(secondsToEvent / 60)) % 60,
+    seconds: Math.max(0, Math.floor(secondsToEvent)) % 60,
+  }
+}
+
+function getTwoSpaceNumber(number: number) {
+  return `${number < 10 ? '0' : ''}${number}`
+}
+
+export function customFormatDuration(duration: Duration, delimiter?: string) {
+  const delimiterSymbol = delimiter ?? ':'
+  const hours = getTwoSpaceNumber(duration.hours ?? 0)
+  const minutes = getTwoSpaceNumber(duration.minutes ?? 0)
+  const seconds = getTwoSpaceNumber(duration.seconds ?? 0)
+  return `${hours}${delimiterSymbol}${minutes}${delimiterSymbol}${seconds}`
+}

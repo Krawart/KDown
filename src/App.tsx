9ecd1a5af7c2c3a9d9182afb18f0886e791f5bcf
Filip Krawiec <FIlip.Krawiec@gmail.com>
2022-05-08 22:31:38 +0200

    feat[google_analytics] GA script included to head

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,11 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import RootPage from './pages'
 
-// @ts-ignore
-import ReactGA from 'react-ga'
-
-const TRACKING_ID = 'G-NLXM8P0231'
-ReactGA.initialize(TRACKING_ID)
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<RootPage />} />
+        <Route path='*' element={<RootPage />} />
       </Routes>
     </BrowserRouter>
   )

b15a1e738ba46e9918c36db672cb4608041c1d4e
Filip Krawiec <FIlip.Krawiec@gmail.com>
2022-05-08 22:46:27 +0200

    feat[support_days] Duration formatting updated

--- src/utils/time-utils.ts
+++ src/utils/time-utils.ts
@@ -1,4 +1,5 @@
 export type Duration = {
+  days: number
   hours: number
   minutes: number
   seconds: number
@@ -7,6 +8,7 @@ export type Duration = {
 export function getDuration(date1: Date, date2: Date) {
   const secondsToEvent = (date1.getTime() - date2.getTime()) / 1000
   return {
+    days: Math.max(0, Math.floor(secondsToEvent / 86400)),
     hours: Math.max(0, Math.floor(secondsToEvent / 3600)) % 24,
     minutes: Math.max(0, Math.floor(secondsToEvent / 60)) % 60,
     seconds: Math.max(0, Math.floor(secondsToEvent)) % 60,
@@ -19,10 +21,11 @@ function getTwoSpaceNumber(number: number) {
 
 export function customFormatDuration(duration: Duration, delimiter?: string) {
   const delimiterSymbol = delimiter ?? ':'
+  const days = duration.days ?? 0
   const hours = getTwoSpaceNumber(duration.hours ?? 0)
   const minutes = getTwoSpaceNumber(duration.minutes ?? 0)
   const seconds = getTwoSpaceNumber(duration.seconds ?? 0)
-  return `${hours}${delimiterSymbol}${minutes}${delimiterSymbol}${seconds}`
+  return `${days > 0 ? `${days}d ` : ''}${hours}${delimiterSymbol}${minutes}${delimiterSymbol}${seconds}`
 }
 
 export function isDurationEqualsZero(duration: Duration) {
